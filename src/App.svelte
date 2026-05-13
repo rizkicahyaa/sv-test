302a36797dbe35f3803f8ad8a592028b56c8c88b
Rizki Cahya <rizki.cahya03@gmail.com>
2026-05-13 18:29:40 +0700

    done

--- src/App.svelte
+++ src/App.svelte
@@ -1,3 +1,8 @@
-<section>
-	<h1 class="text-2xl">Halo</h1>
-</section>
+<header class="flex w-full flex-col items-center justify-center py-5 text-center">
+	<h1 class="mb-3 text-4xl">Hello world!</h1>
+	<p>
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim voluptatibus id voluptates? Magni
+		ducimus ab porro animi rem minus! Sunt delectus nemo eum accusamus dolorem reprehenderit
+		repudiandae quis corrupti aliquam.
+	</p>
+</header>
